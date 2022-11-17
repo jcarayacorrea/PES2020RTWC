867ef1ebe9a75d6598cfa534f059583cd762c2b1
Juan Carlos Araya Correa <jcarayacorrea@gmail.com>
2022-11-17 18:12:39 -0300

    First commit

---
+++ PES2020RTWC/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for PES2020RTWC project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PES2020RTWC.settings')
+
+application = get_asgi_application()
---
+++ sudamerica/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SudamericaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'sudamerica'
