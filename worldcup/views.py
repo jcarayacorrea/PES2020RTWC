7a67be92611726d154b513d2fa820686c73d9c7e
Juan Carlos Araya Correa <jcarayacorrea@gmail.com>
2022-11-30 20:16:50 -0300

    fetch data world cup module

--- worldcup/views.py
+++ worldcup/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
+from utils import db_conexion
+
+
 # Create your views here.
 def maindraw(request):
-    return render(request,'worldcup/maindraw.html')
+    context = {}
+    context['teams'] = getTeams('mainDraw')
+    return render(request,'worldcup/maindraw.html',context)
 
 def playoff(request):
-    return render(request,'worldcup/playoff.html')
+    context = {}
+    context['teams'] = getTeams('playoff')
+    return render(request,'worldcup/playoff.html',context)
+
+def getTeams(stage):
+    db = db_conexion()
+    return db.get_collection('Teams').find({'stage.mainDraw': True}).sort('fifa_nation_rank', 1) if stage == 'mainDraw' else db.get_collection('Teams').find({'stage.playoff': True}).sort('fifa_nation_rank', 1)
