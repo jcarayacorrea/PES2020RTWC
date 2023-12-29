de33afd3dd39c9313bc307ff7c2ef1ff7789e2cf
jcarayacorrea <jcarayacorrea@gmail.com>
2023-12-29 17:17:41 -0300

    random select zone

--- worldcup/views.py
+++ worldcup/views.py
@@ -122,9 +122,8 @@ def setTeamPosition(team, a, b, c, d, e, f, g, h, position, teamsCount, maxLengt
         zones.remove('G')
     if filterConfList(team['conf_name'], h, teamsCount) == True or len(h) == maxLength:
         zones.remove('H')
-    if len(zones) > 1:
-        random.shuffle(zones)
-    insertTeam(team, zones[0], position, a, b, c, d, e, f, g, h)
+
+    insertTeam(team, random.choice(zones), position, a, b, c, d, e, f, g, h)
 
 
 def filterConfList(conf_name, list, count_teams):
