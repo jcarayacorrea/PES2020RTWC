94ec5aa1493e387c2f49a08a37c16a277f2105c4
Juan C. Araya <jcarayacorrea@gmail.com>
2024-03-07 02:57:19 -0300

    adjust win prob

--- MatchSimulator.py
+++ MatchSimulator.py
@@ -11,11 +11,11 @@ def calcular_probabilidad_ganador(ranking_local, ranking_visitante):
             probabilidad_local = 0.7  # Probabilidad de gol local más alta
             probabilidad_visitante = 0.3  # Probabilidad de gol visitante más baja
         elif diferencia_alta(ranking_local, ranking_visitante):
+            probabilidad_local = 0.75  # Probabilidad de gol local más alta
+            probabilidad_visitante = 0.25  # Probabilidad de gol visitante más baja
+        elif diferencia_extrema(ranking_local, ranking_visitante):
             probabilidad_local = 0.8  # Probabilidad de gol local más alta
             probabilidad_visitante = 0.2  # Probabilidad de gol visitante más baja
-        elif diferencia_extrema(ranking_local, ranking_visitante):
-            probabilidad_local = 0.85  # Probabilidad de gol local más alta
-            probabilidad_visitante = 0.15  # Probabilidad de gol visitante más baja
         else:
             probabilidad_local = 0.6  # Probabilidad de gol local más alta
             probabilidad_visitante = 0.4  # Probabilidad de gol visitante más baja
@@ -26,11 +26,11 @@ def calcular_probabilidad_ganador(ranking_local, ranking_visitante):
             probabilidad_local = 0.3  # Probabilidad de gol local más alta
             probabilidad_visitante = 0.7  # Probabilidad de gol visitante más baja
         elif diferencia_alta(ranking_local, ranking_visitante):
+            probabilidad_local = 0.25  # Probabilidad de gol local más alta
+            probabilidad_visitante = 0.75  # Probabilidad de gol visitante más baja
+        elif diferencia_extrema(ranking_local, ranking_visitante):
             probabilidad_local = 0.2  # Probabilidad de gol local más alta
             probabilidad_visitante = 0.8  # Probabilidad de gol visitante más baja
-        elif diferencia_extrema(ranking_local, ranking_visitante):
-            probabilidad_local = 0.15  # Probabilidad de gol local más alta
-            probabilidad_visitante = 0.85  # Probabilidad de gol visitante más baja
         else:
             probabilidad_local = 0.4  # Probabilidad de gol local más alta
             probabilidad_visitante = 0.6  # Probabilidad de gol visitante más baja
