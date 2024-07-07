6319fd1c93310f6561aaad0e3ba6a25a22a4ebfd
Juan C. Araya <jcarayacorrea@gmail.com>
2024-07-07 15:35:16 -0400

    New Copa America Draw

--- sudamerica/views.py
+++ sudamerica/views.py
@@ -3,7 +3,8 @@ import random
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from utils import getTeams, updateStage,getTeamsCopaAmerica
+from operator import itemgetter
+from utils import getTeams, updateStage, getTeamsCopaAmerica
 
 
 # Create your views here.
@@ -12,16 +13,18 @@ def teams(request):
     context['teams'] = getTeams(conf_name='CONMEBOL')
     return render(request, 'sudamerica/teamlist.html', context)
 
+
 def copaAmerica(request):
     context = {}
     context['teams'] = getTeamsCopaAmerica()
     return render(request, 'sudamerica/copaamerica.html', context)
 
 
-def updateProgress(request,id, stage):
+def updateProgress(request, id, stage):
     if request.method == 'POST':
-        updateStage(id,stage)
-    return   redirect('sudamerica.finalround')
+        updateStage(id, stage)
+    return redirect('sudamerica.finalround')
+
 
 def copaAmericaButton(request):
     if request.method == 'GET':
@@ -33,7 +36,6 @@ def copaAmericaButton(request):
         random.shuffle(zone3)
         random.shuffle(zone4)
 
-
         context['zone1'] = zone1
         context['zone2'] = zone2
         context['zone3'] = zone3
@@ -41,20 +43,27 @@ def copaAmericaButton(request):
 
         return render(request, 'sudamerica/copaamerica.html', context)
 
-def copaAmericaDraw(teams):
-    pool1 = [teams[0], teams[1], teams[2], teams[3]]
-    pool2 = [teams[4], teams[5], teams[6], teams[7]]
-    pool3 = [teams[8], teams[9], teams[10], teams[11]]
-    pool4 = [teams[12], teams[13], teams[14], teams[15]]
 
-    random.shuffle(pool1)
+def copaAmericaDraw(teams):
+    sudamerica = [team for team in teams if team['conf_name'] == 'CONMEBOL']
+    ncamerica = [team for team in teams if team['conf_name'] == 'CONCACAF']
+    sudSeed = sudamerica[0:2]
+    ncSeed = ncamerica[0:2]
+    restTeams = sorted(sudamerica[2:10] + ncamerica[2:6], key=itemgetter('fifa_nation_rank'), reverse=False)
+
+    pool2 = restTeams[0:4]
+    pool3 = restTeams[4:8]
+    pool4 = restTeams[8:12]
+
+    random.shuffle(sudSeed)
+    random.shuffle(ncSeed)
     random.shuffle(pool2)
     random.shuffle(pool3)
     random.shuffle(pool4)
 
-    zone1 = [pool1[0], pool2[0], pool3[0], pool4[0]]
-    zone2 = [pool1[1], pool2[1], pool3[1], pool4[1]]
-    zone3 = [pool1[2], pool2[2], pool3[2], pool4[2]]
-    zone4 = [pool1[3], pool2[3], pool3[3], pool4[3]]
+    zone1 = [sudSeed[0], pool2[0], pool3[0], pool4[0]]
+    zone2 = [ncSeed[0], pool2[1], pool3[1], pool4[1]]
+    zone3 = [ncSeed[1], pool2[2], pool3[2], pool4[2]]
+    zone4 = [sudSeed[1], pool2[3], pool3[3], pool4[3]]
 
     return zone1, zone2, zone3, zone4
