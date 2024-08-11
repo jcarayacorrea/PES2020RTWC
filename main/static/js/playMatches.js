fc43c2a466a75aebd422bf3dbb510ddf32079ada
Juan C. Araya <jcarayacorrea@gmail.com>
2024-08-11 01:50:09 -0400

    all exec activity

--- main/static/js/playMatches.js
+++ main/static/js/playMatches.js
@@ -1,5 +1,8 @@
 function playMatches(){
     matchesbtn = document.getElementsByClassName("matches");
+    document.getElementById('btnExec').disabled = true;
+    document.getElementById('rotateOnDiv').style.display = "block";
+    document.getElementById('rotateOffDiv').style.display = "none";
     for (match of matchesbtn){
         match.click();
     }
