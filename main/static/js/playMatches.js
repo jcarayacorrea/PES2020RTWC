2220542326a6642b02593e443ba171d55fbb938c
Juan C. Araya <jcarayacorrea@gmail.com>
2024-08-31 00:04:07 -0400

    Update playMatches.js

--- main/static/js/playMatches.js
+++ main/static/js/playMatches.js
@@ -1,9 +1,31 @@
-function playMatches(){
-    matchesbtn = document.getElementsByClassName("matches");
+async function playMatches() {
+    let promisesArray = arrayToPromise(document.getElementsByClassName("matches"));
     document.getElementById('btnExec').disabled = true;
     document.getElementById('rotateOnDiv').style.display = "block";
     document.getElementById('rotateOffDiv').style.display = "none";
-    for (let i=0;i<matchesbtn.length;i++){
-           matchesbtn.item(i).click();
+    for (let i = 0; i < promisesArray.length; i++) {
+        await execPromises(promisesArray[i]);
     }
+}
+
+function arrayToPromise(matches) {
+    let promiseArray = []
+    for (let i = 0; i < matches.length; i += 2) {
+        promiseArray.push([createPromise(matches.item(i).click(),(i === 0) ? 0 : 20000), createPromise(matches.item(i + 1).click(),(i === 0) ? 0 : 20000)])
+    }
+    return promiseArray;
+}
+
+function createPromise(execLine, waitTime) {
+    return new Promise((resolve) => {
+        setTimeout(()=>{
+           resolve(execLine)
+        },waitTime)
+
+    });
+}
+
+async function execPromises(promArray, ) {
+     return await Promise.all(promArray).then(()=>{console.log('Ejecutando matches....')});
+
 }
