ce342db8c3db529000ffbf2479a8968fa7e07c8e
Juan C. Araya <jcarayacorrea@gmail.com>
2024-09-01 01:51:54 -0400

    Syncronus ajax call

--- main/static/js/playMatches.js
+++ main/static/js/playMatches.js
@@ -1,31 +1,14 @@
-async function playMatches() {
-    let promisesArray = arrayToPromise(document.getElementsByClassName("matches"));
+const playMatches = async () => {
+
     document.getElementById('btnExec').disabled = true;
     document.getElementById('rotateOnDiv').style.display = "block";
     document.getElementById('rotateOffDiv').style.display = "none";
-    for (let i = 0; i < promisesArray.length; i++) {
-        await execPromises(promisesArray[i]);
-    }
-}
 
-function arrayToPromise(matches) {
-    let promiseArray = []
-    for (let i = 0; i < matches.length; i += 2) {
-        promiseArray.push([createPromise(matches.item(i).click(),(i === 0) ? 0 : 20000), createPromise(matches.item(i + 1).click(),(i === 0) ? 0 : 20000)])
-    }
-    return promiseArray;
-}
+   $('#fixtures button').map((index,element)=>{
+      element.click();
+   })
 
-function createPromise(execLine, waitTime) {
-    return new Promise((resolve) => {
-        setTimeout(()=>{
-           resolve(execLine)
-        },waitTime)
 
-    });
-}
-
-async function execPromises(promArray, ) {
-     return await Promise.all(promArray).then(()=>{console.log('Ejecutando matches....')});
 
 }
+
