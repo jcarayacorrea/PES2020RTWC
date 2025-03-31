413e823434fc07b959e99db1f04ac657cd069905
Juan C. Araya <jcarayacorrea@gmail.com>
2025-03-30 21:24:36 -0300

    PlayOff Vibe coding

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('api/teams/', views.teamListApi, name='api.teams'),
+    path('api/teams/', views.get_team_list, name='api.teams'),
     path('fixtures/<str:conf>/<str:round>/<str:zone>', views.fixtureZone, name='zones.fixtures'),
     path('standings/<str:conf>/<str:round>/<str:zone>', views.standingsZone, name='zones.standings'),
     path('match_simulator/<int:fixture>/<int:match>/<str:homeId>/<str:awayId>/<str:conf>/<str:round>/<str:zone>/<int:extraTime>/<int:singleLoad>',
