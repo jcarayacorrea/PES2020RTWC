5c046970a3d2b73bd32804b2b1d7597ea5ee5acc
Juan C. Araya <jcarayacorrea@gmail.com>
2025-04-01 16:18:44 -0300

    Playoff works

--- worldcup/urls.py
+++ worldcup/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('maindraw/', views.maindraw, name='worldcup.maindraw'),
     path('maindraw/draw/', views.draw_main_button, name='worldcup.draw'),
     path('playoff/', views.playoff, name='worldcup.playoff'),
-    path('playoff/draw', views.playoffDraw, name='worldcup.playoff.draw'),
+    path('playoff/draw', views.getPlayoffPage, name='worldcup.playoff.draw'),
 
 ]
