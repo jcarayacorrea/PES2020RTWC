b9bd218e20d586cb5940a6bef38f7ec62ff6f74e
Juan C. Araya <jcarayacorrea@gmail.com>
2025-04-13 17:36:51 -0400

    Bug Fix Draw

--- worldcup/views.py
+++ worldcup/views.py
@@ -32,9 +32,9 @@ def playoff(request):
 
 def shuffle_teams_and_assign_to_context(zones):
     context = {}
-    for i, zone in enumerate(zones):
-        random.shuffle(zones.get(zone))
-        context[f'zone{chr(65 + i)}'] = zones.get(zone)
+    for i in range(len(zones)):
+        random.shuffle(zones[i])
+        context[f'zone{chr(65 + i)}'] = zones[i]
     return context
 
 
@@ -73,60 +73,7 @@ def getPlayoffPage(request):
         return render(request, 'worldcup/playoff.html', context)
 
 
-def assign_teams_to_groups(teams, groups):
-    # Assigns a list of teams to groups
-    pool = teams
-    random.shuffle(pool)
-    for key, team in zip(groups.keys(), pool):
-        groups.get(key).append(team)
-    return groups
 
-
-def draw(teams):
-    groups = {
-        'A': [],
-        'B': [],
-        'C': [],
-        'D': [],
-        'E': [],
-        'F': [],
-        'G': [],
-        'H': []
-    }
-
-    # manually creating pools and assigning them to groups
-    for i in range(4):
-        start_index = i * 8
-        end_index = start_index + 8
-        pool_teams = teams[start_index:end_index]
-        groups = assign_teams_to_groups(pool_teams, groups)
-
-    return groups
-
-
-def set_team_position(team, groups, position, teams_count, max_length):
-    all_zones = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
-    valid_zones = [zone for zone in all_zones if not (
-            filter_conf_list(team['conf_name'], groups.get(zone), teams_count) or len(
-        groups.get(zone)) == max_length)]
-    insertTeam(team, random.choice(valid_zones), position, groups)
-
-
-def is_valid_team_count(count_teams, count_conf):
-    return (count_teams <= 8 and count_conf == 1) or (count_teams > 8 and count_conf == 2)
-
-
-def filter_conf_list(conf_name, team_list, count_teams):
-    count_conf = len([team for team in team_list if conf_name == team['conf_name']])
-    return is_valid_team_count(count_teams, count_conf)
-
-
-def countTeams(conf_name, teamList):
-    return len([team for team in teamList if conf_name == team['conf_name']])
-
-
-def insertTeam(team, zone, pos, groups):
-    groups.get(zone).insert(pos, team)
 
 
 def playoffDraw(teams):
@@ -142,6 +89,62 @@ def playoffDraw(teams):
         pools.append(pool)
 
     return tuple(pools)
+
+def draw(teams):
+    groups = {
+        'A': [],
+        'B': [],
+        'C': [],
+        'D': [],
+        'E': [],
+        'F': [],
+        'G': [],
+        'H': []
+    }
+    pool1 = teams[0:8]
+    pool2 = teams[8:16]
+    pool3 = teams[16:24]
+    pool4 = teams[24:32]
+    random.shuffle(pool1)
+    for key, team in zip(groups.keys(), pool1):
+        groups.get(key).append(team)
+
+    pools = [pool2, pool3, pool4]
+
+    for pool, i in zip(pools, range(2, 5)):
+        random.shuffle(pool)
+        for team in pool:
+            setTeamPosition(team, groups, i - 1, teamsCount=countTeams(team['conf_name'], getTeamsMainDraw()),
+                            maxLength=i)
+    return groups.get('A'), groups.get('B'), groups.get('C'), groups.get('D'), groups.get('E'), groups.get(
+        'F'), groups.get('G'), groups.get('H')
+
+
+def setTeamPosition(team, groups, position, teamsCount, maxLength):
+    zones = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+    for zone in zones.copy():
+        if filterConfList(team['conf_name'], groups.get(zone), teamsCount) == True or len(
+                groups.get(zone)) == maxLength:
+            zones.remove(zone)
+
+    insertTeam(team, random.choice(zones), position, groups)
+
+
+def filterConfList(conf_name, list, count_teams):
+    count_conf = len([team for team in list if conf_name == team['conf_name']])
+    if (count_teams <= 8 and count_conf == 1) or (count_teams > 8 and count_conf == 2):
+        return True
+    return False
+
+
+def countTeams(conf_name, teamList):
+    return len([team for team in teamList if conf_name == team['conf_name']])
+
+
+def insertTeam(team, zone, pos, groups):
+    groups.get(zone).insert(pos, team)
+
+
 
 
 @register.filter
